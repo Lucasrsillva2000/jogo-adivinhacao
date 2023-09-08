641563f9673e1e49783dfd8fe3644f4d7d7815d7
lucas <lucasrsillva2000@gmail.com>
2023-09-08 09:39:20 -0300

    updating source-code

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-//Variaveis
+//varaiveis
 const btnTry = document.querySelector('#btnTry')
 const btnReset = document.querySelector('#btnReset')
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
-
 let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-//EVENTOS
+//eventos
 btnTry.addEventListener('click', handleTryClick)
 btnReset.addEventListener('click', handleResetClick)
-document.addEventListener('keydown', function (e) {
-  if (e.key == 'Enter' && screen1.classList.contains('hide')) {
-    handleResetClick()
-  }
-})
+document.addEventListener('keypress', handleTryKeypress)
 
-// funçôes
+//funções
 function handleTryClick(event) {
-  event.preventDefault() //nao faça o padrão
-
+  event.preventDefault()
   const inputNumber = document.querySelector('#inputNumber')
-  const guess = Number(inputNumber.value)
+
+  if (Number(inputNumber.value) == '') {
+    window.alert('digite algum valor para prosseguir')
+    return
+  }
+
+  if (Number(inputNumber.value) <= 0 || Number(inputNumber.value) > 10) {
+    window.alert('ERRO, DIGITE VALORES ENTRE 1 E 10')
+  }
 
   if (Number(inputNumber.value) == randomNumber) {
     toggleScreen()
-    screen2.querySelector(
-      'h2'
-    ).innerText = `acertou em ${xAttempts}  tentativas.`
+    screen2.querySelector('h2').innerText = `Acertou em ${xAttempts} tentativas`
   }
+
   inputNumber.value = ''
   xAttempts++
 }
@@ -39,7 +40,17 @@ function handleResetClick() {
   randomNumber = Math.round(Math.random() * 10)
 }
 
+function handleTryKeypress(e) {
+  if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
+}
+
 function toggleScreen() {
   screen1.classList.toggle('hide')
   screen2.classList.toggle('hide')
 }
+
+// DESAFIOS
+
+// FAZER UM LIMITE QUE SÓ PODE CLICAR ENTRE 0 E 10
